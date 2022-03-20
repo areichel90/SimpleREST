db34e71295bcf3d101b05a46418a5cd07d5de3cc
Me <andrew.reichel@gmail.com>
2022-03-19 22:48:49 -0400

    created test routes and implemented appro. CRUD methdos

--- flask_test.py
+++ flask_test.py
@@ -1,9 +1,92 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, jsonify
+import datetime
+
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def test():
-    return "This Works"
+# this is to be replaced with a schema that all clients will
+# use to share payloads back and forth between the server
+test_payload = [
+    {"id": "test_payload_1",
+    "author": "macbook_pro",
+    "payload": "testing, testing"}
+]
+
+@app.route("/sandbox", methods=['GET','POST'])
+def sandbox():
+    print("Request: ", request)
+
+    ''' GET Method '''
+    if request.method == "GET":
+        return jsonify(test_payload)
+    else:
+        'Nothing Found', 404
+
+    ''' PUT/POST Method '''
+    if (request.method == "POST" or request.method == "PUT"):
+        new_id = request.form["id"]
+        new_author = request.form["author"]
+        new_payload = request.form["payload"]
+
+        if request.method == "POST":
+            new_entry = {
+                "id":new_id,
+                "author":new_author,
+                "payload":new_payload
+            }
+            test_payload.append(new_entry)
+            return jsonify(test_payload)
+
+
+@app.route('/sandbox/<string:id>', methods=['GET','PUT','POST'])
+def single_entry(id):
+    print("Request: ", request, "ID: ", id)
+
+    ''' GET Method '''
+    if request.method == "GET":
+        for entry in test_payload:
+            if entry["id"] == id:
+                return jsonify(entry)
+    else:
+        'Nothing Found', 404
+
+    ''' PUT/POST Method '''
+    if (request.method == "POST" or request.method == "PUT"):
+        new_id = request.form["id"]
+        new_author = request.form["author"]
+        new_payload = request.form["payload"]
+
+        if request.method == "POST":
+            new_entry = {
+                "id":new_id,
+                "author":new_author,
+                "payload":new_payload
+            }
+            test_payload.append(new_entry)
+            return jsonify(test_payload)
+
+        elif request.method == "PUT":
+            for entry in test_payload:
+                if test_payload[entry] == id:
+                    updated_payload = {
+                        "id":new_id,
+                        "author":new_author,
+                        "title":new_payload 
+                    }
+            return jsonify(updated_payload)
+
+
+
+@app.route('/') 
+def home():
+    return "This Works!"
+
+@app.route("/<name>")
+def print_name(name):
+    return f"Hello {name}"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
