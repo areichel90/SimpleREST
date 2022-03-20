565ba39d3d75ca8269c697a9846af74cd12b1255
Me <andrew.reichel@gmail.com>
2022-03-20 09:39:08 -0400

    sandbox get and post working

---
+++ client_requests.py
@@ -0,0 +1,24 @@
+import requests
+
+def GET(route, verbose=True):
+    if verbose:
+        print(f"GET: {route}")
+    return requests.get(route)
+
+def POST(endpoint, payload, verbose=True):
+    if verbose:
+        print(f"POST: {endpoint}")
+    requests.post(endpoint, data=payload)
+
+if __name__ == "__main__":
+    sandbox_route = "http://127.0.0.1:8000/sandbox"
+    test_payload = {
+        "author": "macbook_PUT", 
+        "id": "test_payload_2", 
+        "payload": "testing, testing?"
+    }
+    sandbox = GET(sandbox_route)
+    POST(sandbox_route, test_payload)
+    sandbox_updated = GET(sandbox_route)
+    print(f"\nPayload: {sandbox.text}\n,\
+        {sandbox_updated.text}" )
